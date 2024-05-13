f7c125fd8bb9800bc2fd42be55f2cb6f4e6d38bc
Carson Turner <73399912+CDanielTurner@users.noreply.github.com>
2024-05-13 15:13:18 -0600

    Week-2 Assignement Commit
    Added week-2 folder and edited the code inside the new project that was created

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+	return (
+		<main>
+			<h1>My name is Carson Turner</h1>
+			<Link href="https://github.com/CDanielTurner/cprg306-assignments">
+				My Github Link
+			</Link>
+		</main>
+	);
+}
